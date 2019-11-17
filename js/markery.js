c854e555f2252b33e26150e983678a048090dd56
Ayazhanochka <57761965+Ayazhanochka@users.noreply.github.com>
2019-11-18 00:01:34 +0300

    Add files via upload

---
+++ js/markery.js
@@ -0,0 +1,48 @@
+var greenIcon = L.icon({
+	iconUrl: 'https://clipart-best.com/img/snowflakes/snowflakes-clip-art-1.png',
+	iconSize: [38, 95], // size of the icon
+	iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+	popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+L.marker([53.419965, 59.001211], {
+	icon: greenIcon
+}).addTo(map);
+var greenIcon = L.icon({
+	iconUrl: 'https://clipart-best.com/img/snowflakes/snowflakes-clip-art-1.png',
+	iconSize: [38, 95], // size of the icon
+	iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+	popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+L.marker([55.1638, 61.4375], {
+	icon: greenIcon
+}).addTo(map);
+var greenIcon = L.icon({
+	iconUrl: 'https://clipart-best.com/img/snowflakes/snowflakes-clip-art-1.png',
+	iconSize: [38, 95], // size of the icon
+	iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+	popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+L.marker([57.92, 59.96], {
+	icon: greenIcon
+}).addTo(map);
+var greenIcon = L.icon({
+	iconUrl: 'https://clipart-best.com/img/snowflakes/snowflakes-clip-art-1.png',
+	iconSize: [38, 95], // size of the icon
+	iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+	popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+L.marker([69.35, 88.20], {
+	icon: greenIcon
+}).addTo(map);
+L.marker([53.4, 59], {
+	icon: greenIcon
+}).bindPopup("Magnitogorsk").addTo(map);
+L.marker([55.1638, 61.4375], {
+	icon: greenIcon
+}).bindPopup("Chelyabinsk").addTo(map);
+L.marker([57.92, 59.96], {
+	icon: greenIcon
+}).bindPopup("Nizhniy Tagil").addTo(map);
+L.marker([69.35, 88.20], {
+	icon: greenIcon
+}).bindPopup("Noril'sk").addTo(map);
